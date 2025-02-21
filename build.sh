6b19c0efc510bd1f1855ac9e32dd8f0013d6f14d
olumidemixx <OLUMIDEMIXX@GMAIL.COM>
2025-02-21 14:54:23 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-
-# Install Chrome in headless mode
-apt-get update
-apt-get install -y wget gnupg2 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-apt-get update
-apt-get install -y google-chrome-stable xvfb
+# Add necessary repositories and install Chrome
+sudo apt-get update
+sudo apt-get install -y wget unzip
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
 
 # Install ChromeDriver
-CHROME_VERSION=$(google-chrome-stable --version | cut -d " " -f3 | cut -d "." -f1)
-CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-wget -q -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | awk -F'.' '{print $1}')
+wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION}/chromedriver_linux64.zip
 unzip /tmp/chromedriver.zip -d /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
 
-# Install Python dependencies
+# Install Python requirements
 pip install -r requirements.txt
-
-# Create directory for Chrome if it doesn't exist
-mkdir -p /tmp/.chrome
