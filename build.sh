d13d925bf8206812070e7e9602a3280925e4ebba
olumidemixx <OLUMIDEMIXX@GMAIL.COM>
2025-02-21 17:07:00 +0100

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# First, fix potential Windows line endings in this script
+sed -i 's/\r$//' build.sh
+
+# Download and install Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+apt-get update -y
+apt-get install -y google-chrome-stable
+
+# Install Chrome Driver
+CHROME_VERSION=$(google-chrome --version | awk '{ print $3 }' | awk -F'.' '{ print $1 }')
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget -N "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+mv -f chromedriver /usr/local/bin/chromedriver
+
+# Install Python dependencies
+pip install -r requirements.txt
