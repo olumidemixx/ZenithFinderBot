a51d01b539e5b00799f41c4149c4bafd71e5374d
olumidemixx <OLUMIDEMIXX@GMAIL.COM>
2025-02-21 17:42:07 +0100

    Update onlygatekeptFINDALL2.py

--- onlygatekeptFINDALL2.py
+++ onlygatekeptFINDALL2.py
@@ -29,6 +29,7 @@ def zenithfinderbot(list_of_tokens):
     all_addresses=[]
     for token in list_of_tokens:
         options = webdriver.ChromeOptions()
+        options.binary_location = "/usr/bin/google-chrome"
         options.add_argument("--headless=new")  # Use the new headless mode
 
         # Initialize the Chrome driver using ChromeDriverManager
