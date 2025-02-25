bad87f07c3cde3c4fc4a70684bb8b11276152f32
olumidemixx <OLUMIDEMIXX@GMAIL.COM>
2025-02-25 10:48:51 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 CHROMEDRIVER_URL="https://storage.googleapis.com/chrome-for-testing-public/133.0.6943.126/linux64/chromedriver-linux64.zip"
-
+pip install -r requirements.txt
 # Add verification to check if Chrome is already installed and working
 if [[ -d $STORAGE_DIR/chrome ]] && [[ -f $STORAGE_DIR/chrome/opt/google/chrome/chrome ]]; then
   echo "Verifying Chrome installation..."
